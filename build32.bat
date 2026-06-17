b854726935037d4969149583651f71fd12329435
radian <12453432+radian-jp@users.noreply.github.com>
2026-06-18 02:08:39 +0900

    Add comment

--- build32.bat
+++ build32.bat
@@ -1,3 +1,7 @@
+setlocal
+@rem turbojpegƒrƒ‹ƒh‚É¸”s‚·‚éê‡‚Éİ’è‚·‚é
+@rem set CMAKE_GENERATOR=NMake Makefiles
 cargo build --target=i686-pc-windows-msvc --release
 @rem cargo build --target=x86_64-pc-windows-msvc --release
+endlocal
 pause
--- build64.bat
+++ build64.bat
@@ -1,3 +1,7 @@
+setlocal
+@rem turbojpegƒrƒ‹ƒh‚É¸”s‚·‚éê‡‚Éİ’è‚·‚é
+@rem set CMAKE_GENERATOR=NMake Makefiles
 @rem cargo build --target=i686-pc-windows-msvc --release
 cargo build --target=x86_64-pc-windows-msvc --release
+endlocal
 pause
