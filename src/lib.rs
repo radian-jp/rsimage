911f0ded6f82bd8aed3075665eac3e61b3566781
radian <12453432+radian-jp@users.noreply.github.com>
2026-07-04 17:49:21 +0900

    png_decoder廃止

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 mod rsimage_common;
-mod png_decoder;
 mod jpg_decoder;
 mod generic_decoder;
 
 pub use rsimage_common::*;
-pub use png_decoder::*;
 pub use jpg_decoder::*;
 pub use generic_decoder::*;
